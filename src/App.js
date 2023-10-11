f16916b84138d0e4499690e5807c5276b73cefd2
Subramanian <48426449+Subramanian1503@users.noreply.github.com>
2023-10-11 16:48:25 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+// Task-1
+// Create a Navbar components for the links of all the pages then uncomment the code gven below..
+// import Navbar from "./components/Navbar";
+
+// Task-2
+// Create a Router with following Routes using react-router-dom. After that uncomment the code.
+// import { Fof, Home, Login, Signup, Profile } from "./pages";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Router>
+        
+      </Router> */}
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+// Task-1
+// import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return <nav>{/* Create the Navbar here */}</nav>;
+};
+
+export default Navbar;
